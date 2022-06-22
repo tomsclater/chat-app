0b73867727fd772013325d5163d5c4d65e6244d4
Thomas <tomsclater@live.com>
2022-06-22 00:42:39 -0700

    Beginning Chat Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+const apiKey = "4bqntuauydyh";
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>Streaming Chat App</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 };
---
+++ client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChannelListContainer = () => {
+  return <div>ChannelListContainer</div>;
+};
+
+export default ChannelListContainer;
