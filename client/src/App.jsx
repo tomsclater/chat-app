b1990fa72e19c7fc63ba72f2c608d7631e4860a9
Thomas <tomsclater@live.com>
2022-06-22 02:12:03 -0700

    Added styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Cookies from "universal-cookie";
 
 import { ChannelListContainer, ChannelContainer } from "./components";
 
+import "./App.css";
+
 const apiKey = "4bqntuauydyh";
 
 const client = StreamChat.getInstance(apiKey);
