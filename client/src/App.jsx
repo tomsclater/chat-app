bfa6bb217716a7b36cde39325448dfbf2a029d9f
Thomas <tomsclater@live.com>
2022-06-22 02:00:17 -0700

    Setting up imports and folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
+import { ChannelListContainer, ChannelContainer } from "./components";
+
 const apiKey = "4bqntuauydyh";
 
 const client = StreamChat.getInstance(apiKey);
