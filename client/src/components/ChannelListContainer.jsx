bf8a5dd7a91424906d0f3afcc8be70ae37faece0
Thomas <tomsclater@live.com>
2022-06-24 00:24:00 -0700

    Logout button working

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -6,7 +6,9 @@ import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
 import StreamingIcon from "../assets/live-streaming.png";
 import LogoutIcon from "../assets/logout.png";
 
-const SideBar = () => (
+const cookies = new Cookies();
+
+const SideBar = ({ logout }) => (
   <div className="channel-list__sidebar">
     <div className="channel-list__sidebar__icon1">
       <div className="icon1__inner">
@@ -14,7 +16,7 @@ const SideBar = () => (
       </div>
     </div>
     <div className="channel-list__sidebar__icon2">
-      <div className="icon1__inner">
+      <div className="icon1__inner" onClick={logout}>
         <img src={LogoutIcon} alt="Logout" width="30" />
       </div>
     </div>
@@ -23,14 +25,26 @@ const SideBar = () => (
 
 const CompanyHeader = () => (
   <div className="channel-list__header">
-    <p className="channel-list__header__text">Streaming Chat App</p>
+    <p className="channel-list__header__text">Messages</p>
   </div>
 );
 
 const ChannelListContainer = () => {
+  const logout = () => {
+    cookies.remove("userId");
+    cookies.remove("username");
+    cookies.remove("fullName");
+    cookies.remove("avatarURL");
+    cookies.remove("hashedPassword");
+    cookies.remove("phoneNumber");
+    cookies.remove("token");
+
+    window.location.reload();
+  };
+
   return (
     <>
-      <SideBar />
+      <SideBar logout={logout} />
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
         <ChannelSearch />
