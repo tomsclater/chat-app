b0b80102723b96a862db5e7de58a7ebfd2ff9a10
Thomas <tomsclater@live.com>
2022-06-26 01:32:52 -0700

    Added headers

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -2,8 +2,30 @@ import React, { useEffect, useState } from "react";
 import { Avatar, useChatChannel } from "stream-chat-react";
 import { InviteIcon } from "../assets";
 
+const ListContainer = ({ children }) => {
+  return (
+    <div className="user-list__container">
+      <div className="user-list__header">
+        <p>User</p>
+        <p>Invite</p>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+const UserItem = () => {
+  return (
+    <div className="user-item__wrapper">
+      <div className="user-item__name-wrapper">
+        <Avatar />
+      </div>
+    </div>
+  );
+};
+
 const UserList = () => {
-  return <div>UserList</div>;
+  return <ListContainer>UserList</ListContainer>;
 };
 
 export default UserList;
