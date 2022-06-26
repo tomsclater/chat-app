afffd442a79b8a33895ff3c0ac5ffa1a5134dff9
Thomas <tomsclater@live.com>
2022-06-26 01:48:51 -0700

    Added avatars in user list

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, useChatChannel } from "stream-chat-react";
+import { Avatar, useChatContext } from "stream-chat-react";
 import { InviteIcon } from "../assets";
 
 const ListContainer = ({ children }) => {
@@ -14,18 +14,60 @@ const ListContainer = ({ children }) => {
   );
 };
 
-const UserItem = () => {
+const UserItem = ({ user }) => {
   return (
     <div className="user-item__wrapper">
       <div className="user-item__name-wrapper">
-        <Avatar />
+        <Avatar image={user.image} name={user.fullName || user.id} size={32} />
       </div>
     </div>
   );
 };
 
 const UserList = () => {
-  return <ListContainer>UserList</ListContainer>;
+  const { client } = useChatContext();
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [listEmpty, setListEmpty] = useState(false);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      if (loading) return;
+
+      setLoading(true);
+
+      try {
+        const response = await client.queryUsers(
+          { id: { $ne: client.userID } },
+          { id: 1 },
+          { limit: 8 }
+        );
+
+        if (response.users.length) {
+          setUsers(response.users);
+        } else {
+          setListEmpty(true);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+
+    if (client) getUsers();
+  }, []);
+
+  return (
+    <ListContainer>
+      {loading ? (
+        <div className="user-list__message">Loading users ...</div>
+      ) : (
+        users?.map((user, i) => (
+          <UserItem index={i} key={user.id} user={user} />
+        ))
+      )}
+    </ListContainer>
+  );
 };
 
 export default UserList;
