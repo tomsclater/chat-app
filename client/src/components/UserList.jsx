6dc7af4ac527cb0ece23ace38647d6e49afe5cb5
Thomas <tomsclater@live.com>
2022-06-26 20:48:58 -0700

    Added Invite icons

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -15,14 +15,21 @@ const ListContainer = ({ children }) => {
 };
 
 const UserItem = ({ user }) => {
+  const [selected, setSelected] = useState(false);
+
+  const handleSelect = () => {
+    setSelected((prevSelected) => !prevSelected);
+  };
+
   return (
-    <div className="user-item__wrapper">
+    <div className="user-item__wrapper" onClick={handleSelect}>
       <div className="user-item__name-wrapper">
         <Avatar image={user.image} name={user.fullName || user.id} size={32} />
         <p className="user-item__name">
           {user.first_name + " " + user.last_name || user.fullName}
         </p>
       </div>
+      {selected ? <InviteIcon /> : <div className="user-item__invite-empty" />}
     </div>
   );
 };
