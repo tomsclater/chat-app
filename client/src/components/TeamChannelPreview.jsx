46eb96b88046a6098c5938d65f7ea230f2f69b6c
Thomas <tomsclater@live.com>
2022-06-27 01:12:45 -0700

    Can switch between channels in dash

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Avatar, useChatContext } from "stream-chat-react";
 
-const TeamChannelPreview = ({ channel, type }) => {
+const TeamChannelPreview = ({
+  setActiveChannel,
+  setIsCreating,
+  setIsEditing,
+  setToggleContainer,
+  channel,
+  type,
+}) => {
   const { channel: activeChannel, client } = useChatContext();
 
   const ChannelPreview = () => (
@@ -12,17 +19,19 @@ const TeamChannelPreview = ({ channel, type }) => {
 
   const DirectPreview = () => {
     const members = Object.values(channel.state.members).filter(
-      ({ user }) => user.id != client.userID
+      ({ user }) => user.id !== client.userID
     );
+
+    console.log(members[0]);
 
     return (
       <div className="channel-preview__item single">
         <Avatar
           image={members[0]?.user?.image}
-          name={members[0]?.user?.name}
+          name={members[0]?.user?.name || members[0]?.user?.id}
           size={24}
         />
-        <p>{members[0]?.user?.name}</p>
+        <p>{members[0]?.user?.name || members[0]?.user?.id}</p>
       </div>
     );
   };
@@ -35,7 +44,12 @@ const TeamChannelPreview = ({ channel, type }) => {
           : "channel-preview__wrapper"
       }
       onClick={() => {
-        console.log(channel);
+        setIsCreating(false);
+        setIsEditing(false);
+        setActiveChannel(channel);
+        if (setToggleContainer) {
+          setToggleContainer((prevState) => !prevState);
+        }
       }}
     >
       {type === "team" ? <ChannelPreview /> : <DirectPreview />}
