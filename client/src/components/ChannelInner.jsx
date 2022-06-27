222991f6284b9bf72fb56afdeaa98496f9f5949b
Thomas <tomsclater@live.com>
2022-06-27 00:25:31 -0700

    Updating names to username in dash

--- client/src/components/ChannelInner.jsx
+++ client/src/components/ChannelInner.jsx
@@ -68,11 +68,11 @@ const TeamChannelHeader = ({ setIsEditing }) => {
             <div key={i} className="team-channel-header__name-multi">
               <Avatar
                 image={user.image}
-                name={user.fullName || user.id}
+                name={user.name || user.first_name}
                 size={32}
               />
               <p className="team-channel-header__name user">
-                {user.fullName || user.id}
+                {user.name || user.first_name}
               </p>
             </div>
           ))}
--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -19,10 +19,10 @@ const TeamChannelPreview = ({ channel, type }) => {
       <div className="channel-preview__item single">
         <Avatar
           image={members[0]?.user?.image}
-          name={members[0]?.user?.fullName}
+          name={members[0]?.user?.name}
           size={24}
         />
-        <p>{members[0]?.user?.fullName}</p>
+        <p>{members[0]?.user?.name}</p>
       </div>
     );
   };
