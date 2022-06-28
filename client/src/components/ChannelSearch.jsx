1203bb27bc5c2843075fdbc60411deb33dd71a04
Thomas <tomsclater@live.com>
2022-06-27 22:30:54 -0700

    Search functionality added

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,15 +1,42 @@
 import React, { useState, useEffect } from "react";
 import { useChatContext } from "stream-chat-react";
 
+import { ResultsDropdown } from "./";
 import { SearchIcon } from "../assets";
 
-const ChannelSearch = () => {
+const ChannelSearch = ({ setToggleContainer }) => {
+  const { client, setActiveChannel } = useChatContext();
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
+  const [teamChannels, setTeamChannels] = useState([]);
+  const [directChannels, setDirectChannels] = useState([]);
+
+  useEffect(() => {
+    if (!query) {
+      setTeamChannels([]);
+      setDirectChannels([]);
+    }
+  }, [query]);
 
   const getChannels = async (text) => {
     try {
-      // TODO: fetch channels
+      const channelResponse = client.queryChannels({
+        type: "team",
+        name: { $autocomplete: text },
+        members: { $in: [client.userID] },
+      });
+      const userResponse = client.queryUsers({
+        id: { $ne: client.userID },
+        name: { $autocomplete: text },
+      });
+
+      const [channels, { users }] = await Promise.all([
+        channelResponse,
+        userResponse,
+      ]);
+
+      if (channels.length) setTeamChannels(channels);
+      if (users.length) setDirectChannels(users);
     } catch (error) {
       setQuery("");
     }
@@ -21,6 +48,11 @@ const ChannelSearch = () => {
     setLoading(true);
     setQuery(event.target.value);
     getChannels(event.target.value);
+  };
+
+  const setChannel = (channel) => {
+    setQuery("");
+    setActiveChannel(channel);
   };
 
   return (
@@ -37,6 +69,16 @@ const ChannelSearch = () => {
           onChange={onSearch}
         />
       </div>
+      {query && (
+        <ResultsDropdown
+          teamChannels={teamChannels}
+          directChannels={directChannels}
+          loading={loading}
+          setChannel={setChannel}
+          setQuery={setQuery}
+          setToggleContainer={setToggleContainer}
+        />
+      )}
     </div>
   );
 };
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -9,3 +9,4 @@ export { default as EditChannel } from "./EditChannel";
 export { default as TeamMessage } from "./TeamMessage";
 export { default as Auth } from "./Auth";
 export { default as UserList } from "./UserList";
+export { default as ResultsDropdown } from "./ResultsDropdown";
